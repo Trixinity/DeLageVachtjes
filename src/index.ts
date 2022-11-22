e7eb890e759355eb2d3be8ddb9865d6e4ad2a7dd
Trixinity <billvi2002@hotmail.nl>
2022-11-23 00:02:39 +0100

    updated index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { SapphireClient } from '@sapphire/framework';
+import { readFile } from 'node:fs/promises';
 
 const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 
-client.login('your-token-goes-here');
+const content = await readFile('./config.json', { encoding: 'utf8' })
+const config = JSON.parse(content);
+
+client.login(config.token);
