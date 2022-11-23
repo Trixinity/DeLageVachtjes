8a6b106799d39dc7d4f0aa0d4f91e18623a5970d
Calli <me@calli.dev>
2022-11-24 00:09:08 +0100

    chore: fixed the build and run scripts and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { SapphireClient } from '@sapphire/framework';
 import { readFile } from 'node:fs/promises';
 
-const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+async function main() {
+	const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 
-const content = await readFile('./config.json', { encoding: 'utf8' })
-const config = JSON.parse(content);
+	const content = await readFile('./config.json', { encoding: 'utf8' });
+	const config = JSON.parse(content);
 
-client.login(config.token);
+	await client.login(config.token);
+}
+
+main().catch((err) => console.error(err));
