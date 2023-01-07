c656c17d408855886559c9634e9b8c00136efc63
Trixinity <billvi2002@hotmail.nl>
2023-01-07 23:53:44 +0100

    Added deletelistener

--- src/listeners/messageDelete.ts
+++ src/listeners/messageDelete.ts
@@ -1,7 +1,8 @@
 import { Listener, Events } from '@sapphire/framework';
-//import { Message, MessageEmbed } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
+import { formatDate, limitLength } from '../utils/formatting.js';
 
-//const logging_channel_id = '1044029617507401868'
+const logging_channel_id = '1044029617507401868'
 
 export class DeleteListener extends Listener<typeof Events.MessageDelete> {
     public constructor(context: Listener.Context, options: Listener.Options) {
@@ -10,7 +11,29 @@ export class DeleteListener extends Listener<typeof Events.MessageDelete> {
             event: 'messageDelete'
         });
     }
-    public async run() {
+    public async run(message: Message) {
+        if (!message.guild) return
+
+        if (message.author.bot) return
+
+        const channel = this.container.client.channels.resolve(logging_channel_id)
+        if (!channel || !channel.isText()) throw new Error("Can't find the message logging channel")
+
+        let embed = new MessageEmbed()
+            .setTitle('Bericht is verwijderd')
+            .setDescription('**Verwijderd bericht:**\n' + limitLength(message.content))
+            .setColor('#ff9933')
+            .setAuthor({
+                iconURL: message.author.avatarURL() ?? undefined,
+                name: message.author.tag
+            })
+            .setFooter({text: formatDate(new Date())});
+        
+        if (message.attachments.size >= 1 || message.embeds.length >= 1) {
+            embed = embed.addFields({name: 'Er was een bijlage verwijderd', value: `Er waren ${message.attachments.size} bijlagen en ${message.embeds.length} embeds verwijderd.`});
+        }
+
+        await channel.send({embeds: [embed]})
 
     }
 }
