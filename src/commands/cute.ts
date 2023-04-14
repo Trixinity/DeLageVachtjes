1833213e618552955f1cbb7dabc6b79ce9e7d400
Bill Vi <billvi2002@hotmail.nl>
2023-04-14 20:59:39 +0200

    Nederlands please

--- src/commands/cute.ts
+++ src/commands/cute.ts
@@ -36,7 +36,7 @@ export class CuteCommand extends Command {
 		}
 		let userToCheck = interaction.options.getUser('user', true).id;
 		if (userToCheck = '210335930706427906') {
-			return interaction.reply('We weten dat Trixi een cute kleine draakje is smh.')
+			return interaction.reply('We weten dat Trixi een cute klein draakje is smh.')
 		}
         const trixiconURL = "https://media.discordapp.net/attachments/462564425866412045/1035666899389919353/20210806_005557.jpg?width=466&height=466";
 		const percentage = this.getRandomInt(101);
