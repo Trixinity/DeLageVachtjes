916814627771ea068c4241301e0e5e82470ebaf0
Trixinity <billvi2002@hotmail.nl>
2023-05-06 01:09:05 +0200

    Added ability to ask question 8ball cmd

--- src/commands/8ball.ts
+++ src/commands/8ball.ts
@@ -10,27 +10,34 @@ export class EightballCommand extends Command {
 	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
 		console.log('registering eightball command');
 		registry.registerChatInputCommand((builder) =>
-			builder.setName('8ball').setDescription('laat Trixie je vragen beantwoorden'),
-		);
+			builder.setName('8ball').setDescription('laat Trixie je vragen beantwoorden')
+            .addStringOption((option) =>
+                option
+                    .setName('description')
+                    .setDescription('Stel je vraag')
+                    .setRequired(true)
+            )
+        );
 	}
     public getRandomInt(max: number) {
         return Math.floor(Math.random() * max);
     }
     public async chatInputRun(interaction: Command.ChatInputInteraction) {
-        //constants
-        const trixiconURL = "https://media.discordapp.net/attachments/462564425866412045/1035666899389919353/20210806_005557.jpg?width=466&height=466";
-
-        const responses = ['Het is vrij zeker.','Zonder twijfel.', 'Jazeker.', 'Je kan er op rekenen.', 'Zoals ik het zie, ja.', 'Hoogstwaarschijnlijk.',
-            'Ja.', 'Signalen geven een ja.', 'Het ziet er naar uit.', 'Het is beslist zo.', 'Vraag later nog een keer.', 'Ik kan het beter niet nu zeggen.',
-            'Ik kan het nu niet voorspellen.', 'Concentreer je en vraag nog een keer.', 'Ik weet het niet zeker, vraag nog een keer.', 'Reken er niet op.',
-            'Mijn antwoord is nee.', 'Mijn bronnen zeggen van niet.', 'Uitkomst lijkt niet positief.', 'Ik betwijfel het.'];
-
         //We can also debate if we also make this possible to run in an DM
         if (interaction.guild === null) {
             return interaction.reply('Je runt dit niet in een server.')
         }
+        //constants
+        const trixiconURL = "https://media.discordapp.net/attachments/462564425866412045/1035666899389919353/20210806_005557.jpg?width=466&height=466";
+        const responses = ['Het is vrij zeker.','Zonder twijfel.', 'Jazeker.', 'Je kan er op rekenen.', 'Zoals ik het zie, ja.', 'Hoogstwaarschijnlijk.',
+                    'Ja.', 'Signalen geven een ja.', 'Het ziet er naar uit.', 'Het is beslist zo.', 'Vraag later nog een keer.', 'Ik kan het beter niet nu zeggen.',
+                    'Ik kan het nu niet voorspellen.', 'Concentreer je en vraag nog een keer.', 'Ik weet het niet zeker, vraag nog een keer.', 'Reken er niet op.',
+                    'Mijn antwoord is nee.', 'Mijn bronnen zeggen van niet.', 'Uitkomst lijkt niet positief.', 'Ik betwijfel het.'];
+                
+        const user = interaction.user.id;
+        let beschrijving = interaction.options.getString('description', true);
         const embed = new MessageEmbed()
-            .setDescription(responses[this.getRandomInt(20)])
+            .setDescription(`<@${user}> vraagt: \n${beschrijving} \nTR1-X13 antwoordt: \n${responses[this.getRandomInt(20)]}`)
             .setColor([149, 120, 162])
 			.setFooter({text: "Bot gemaakt en beheerd door Trixinity#0164", iconURL: trixiconURL});
 
